e6f3c1487f91e4ef99cb9fbbac6f5e15b67357cd
vukmilenkovic <vukmilenkovic9@gmail.com>
2025-12-26 15:23:09 +0100

    Made the changes to the object and the dom displaying object values, replaced the read with a ratinga.

--- index.js
+++ index.js
@@ -1,56 +1,65 @@
 const myLibrary = [];
 
-function Book(title, author, pages, read, id) {
+function Book(title, author, pages, rating, id) {
+    this.id = id;
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = read;
-    this.id = id;
+    this.rating = rating;
+
 };
 
 function addBookToLibrary(title, author, pages, read, id){
     return new Book(title, author, pages, read, id);
 }
 
-book_001 = addBookToLibrary('Shinning', 'Stephen King', '278', true, crypto.randomUUID());
-book_002 = addBookToLibrary('It', 'Stephen King', '1138', true, crypto.randomUUID());
-book_003 = addBookToLibrary('1984', 'George Orwell', '328', true, crypto.randomUUID());
-book_004 = addBookToLibrary('Animal Farm', 'George Orwell', '112', false, crypto.randomUUID());
-book_005 = addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '310', true, crypto.randomUUID());
-book_006 = addBookToLibrary('The Lord of the Rings', 'J.R.R. Tolkien', '1178', true, crypto.randomUUID());
-book_007 = addBookToLibrary('Fahrenheit 451', 'Ray Bradbury', '194', false, crypto.randomUUID());
-book_008 = addBookToLibrary('Brave New World', 'Aldous Huxley', '268', true, crypto.randomUUID());
-book_009 = addBookToLibrary('Dune', 'Frank Herbert', '412', true, crypto.randomUUID());
-book_010 = addBookToLibrary('The Catcher in the Rye', 'J.D. Salinger', '277', false, crypto.randomUUID());
+book_001 = addBookToLibrary('Shinning', 'Stephen King', '278', 8.7, crypto.randomUUID());
+book_002 = addBookToLibrary('It', 'Stephen King', '1138', 8.5, crypto.randomUUID());
+book_003 = addBookToLibrary('1984', 'George Orwell', '328', 8.8, crypto.randomUUID());
+book_004 = addBookToLibrary('Animal Farm', 'George Orwell', '112', 8.0, crypto.randomUUID());
+book_005 = addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', '310', 9.3, crypto.randomUUID());
+book_006 = addBookToLibrary('The Lord of the Rings', 'J.R.R. Tolkien', '1178', 9.4, crypto.randomUUID());
+book_007 = addBookToLibrary('Fahrenheit 451', 'Ray Bradbury', '194', 7.3, crypto.randomUUID());
+book_008 = addBookToLibrary('Brave New World', 'Aldous Huxley', '268', 7.7, crypto.randomUUID());
+book_009 = addBookToLibrary('Dune', 'Frank Herbert', '412', 8.7, crypto.randomUUID());
+book_010 = addBookToLibrary('The Catcher in the Rye', 'J.D. Salinger', '277', 8.1, crypto.randomUUID());
 
 
 
 myLibrary.push(book_001, book_002, book_003, book_004, book_005, book_006, book_007, book_008, book_009, book_010);
 
 
-
+const container = document.querySelector('.container');
 
 // Display data from JavaScript 
 
-// const books = document.querySelector('.books');
+const books = document.querySelector('.books');
 
-// myLibrary.forEach(bookData => {
-//     const book = document.createElement('div');
-//     book.className = 'book';
-//     book.innerHTML = `
-//         <p>Title:<h3> ${bookData.title}</h3></p>
-//         <p>Author:<h3> ${bookData.author}</h3></p>
-//         <p>Pages:<h3> ${bookData.pages}</h3></p>
-//         <p>I read it?<h3> ${bookData.read}</h3></p>
-//     `;
-//     book.style.border = '5px solid black';
-//     book.style.borderRadius = '12px';
-//     book.style.backgroundColor = 'white';
-//     book.style.fontSize = '24px';
-//     book.style.padding = '20px';
+myLibrary.forEach(bookData => {
+    const div = document.createElement('div');
+    div.className = 'book';
+        div.innerHTML = `
+            <p data-id="${bookData.id}">Title:<h3> ${bookData.title}</h3></p>
+            <p>Author:<h3> ${bookData.author}</h3></p>
+            <p>Pages:<h3> ${bookData.pages}</h3></p>
+            <p>Rating<h3> ${bookData.rating}</h3></p>
+            <button>Remove from library</button>
+        `; 
+    div.style.border = '5px solid black';
+    div.style.borderRadius = '12px';
+    div.style.backgroundColor = 'white';
+    div.style.fontSize = '24px';
+    div.style.padding = '20px';
 
-//     books.appendChild(book);
-// })
+    books.appendChild(div);
+})
+
+// Delete book
+function deleteBook(){
+    console.log(myLibrary);
+
+
+}
 
 
 
@@ -60,31 +69,33 @@ myLibrary.push(book_001, book_002, book_003, book_004, book_005, book_006, book_
 // book to it
 
 // Display data from the database 
-fetch('./db.json')
-    .then(res => res.json())
-    .then(data => {
-        // Request calls the function
-        displayBooks(data.books);
-    })
+// fetch('./db.json')
+//     .then(res => res.json())
+//     .then(data => {
+//         // Request calls the function
+//         displayBooks(data.books);
+//     })
     
 
 
-function displayBooks(books){
-    const container = document.querySelector('.books');
+// function displayBooks(books){
+//     const container = document.querySelector('.books');
 
-    books.forEach(book => {
-        const div = document.createElement('div');
-        div.className = 'book';
-        div.innerHTML = `
-            <p>Title:<h3> ${book.title}</h3></p>
-            <p>Author:<h3> ${book.author}</h3></p>
-            <p>Pages:<h3> ${book.pages}</h3></p>
-            <p>Rating<h3> ${book.rating}</h3></p>
-        `;  
-
-        container.appendChild(div);
+//     books.forEach(book => {
+//         const div = document.createElement('div');
+//         div.className = 'book';
+//         div.innerHTML = `
+//             <p>Title:<h3> ${book.title}</h3></p>
+//             <p>Author:<h3> ${book.author}</h3></p>
+//             <p>Pages:<h3> ${book.pages}</h3></p>
+//             <p>Rating<h3> ${book.rating}</h3></p>
+//             <button>Remove from library</button>
+//         `;  
         
-})  
+
+//         container.appendChild(div);
+        
+// })  
  
 
 const button = document.createElement('div');
@@ -94,7 +105,7 @@ button.innerHTML = `
 `;
 
         
-container.appendChild(button); 
+books.appendChild(button); 
 
 let isOpen = false;
 
@@ -108,7 +119,7 @@ button.addEventListener('click', () => {
     }
 });
 
-}
+
 
 function displayForm(){
     const books = document.querySelector('.books');
@@ -147,10 +158,6 @@ function displayForm(){
         </form>
     `;
         container.appendChild(div);
-
-
-
-
 }
 
 function hideForm() {
